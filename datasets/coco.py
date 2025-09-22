2a150d763a3c8f16443149c8aae9d6ea81830408
Qasem Alharbi <alha0203@vandals.uidaho.edu>
2025-09-22 11:55:39 -0700

    remove data augmentaion

--- datasets/coco.py
+++ datasets/coco.py
@@ -195,21 +195,24 @@ def make_coco_transforms(image_set, args):
 
     if image_set == 'train':
         return T.Compose([
-            T.RandomHorizontalFlip(),
-            T.RandomSelect(
-                T.RandomResize(scales, max_size=1333),
-                T.Compose([
-                    T.RandomResize([400, 500, 600]),
-                    T.RandomSizeCrop(384, 600),
-                    T.RandomResize(scales, max_size=1333),
-                ])
-            ),
-            normalize,
-        ])
+            T.RandomResize([224]),
+            normalize,])
+        #return T.Compose([
+         #   T.RandomHorizontalFlip(),
+          #  T.RandomSelect(
+           #     T.RandomResize(scales, max_size=1333),
+            #    T.Compose([
+             #       T.RandomResize([400, 500, 600]),
+              #      T.RandomSizeCrop(384, 600),
+               #     T.RandomResize(scales, max_size=1333),
+                #])
+           # ),
+            #normalize,
+       # ])
 
     if image_set == 'val':
         return T.Compose([
-            T.RandomResize([800], max_size=1333),
+            T.RandomResize([800], max_size=224),
             normalize,
         ])
 
